cae92e19c4fa66f92376cab500464888840c0b77
geovanens <geovane.silva@ccc.ufcg.edu.br>
2018-08-25 15:42:25 -0300

    server

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
+
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+app.use(history);
+
+var port = process.env.PORT || 8080;
+
+app.listen(port);
